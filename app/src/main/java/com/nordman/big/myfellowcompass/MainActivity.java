8e5c4f62f9d15a725b108f49a8db631004f87932
BigNordman <sergei.vershinin@gmail.com>
2016-03-09 15:35:35 +0500

    Initial commit

---
+++ app/src/main/java/com/nordman/big/myfellowcompass/MainActivity.java
@@ -0,0 +1,75 @@
+package com.nordman.big.myfellowcompass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView info;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main);
+        info = (TextView)findViewById(R.id.info);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("LOG", "login successfull");
+                info.setText(
+                        "User ID: " + loginResult.getAccessToken().getUserId()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("LOG", "login cancelled");
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("LOG", "Login error" + error.getLocalizedMessage());
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+}
